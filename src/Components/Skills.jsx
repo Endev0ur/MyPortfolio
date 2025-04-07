d0560252a48694947a543297736a2cb3ea1b14fc
Endev0ur <devlpr.shubham@gmail.com>
2025-04-07 11:32:18 +0530

    Skills section modified

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,31 +1,53 @@
 import React from 'react'
+import { SiLeetcode , SiCodechef , SiCodeforces , SiGeeksforgeeks} from "react-icons/si";
+import { FaCode } from "react-icons/fa";
 
 const Skills = () => {
   return (
-    <div className='bg-black pb-10'>
-      <h1 className='text-4xl text-white p-10 font-bold'>SKILLS : </h1>
-      <div className='bg-black  ml-5 mr-5 sm:ml-10 sm:mr-10 md:ml-20 md:mr-20 lg:ml-40 lg:mr-40 p-10 text-white border flex items-center justify-around flex-column overflow-scroll rounded-2xl scrollbar-hide'>
-        
-        <div className='text-3xl font-bold p-3 rounded-xl ml-4 border-2 border-violet-400 '>
-          C++
-        </div>
-        <div className='text-3xl font-bold p-3 border-2 rounded-xl ml-4 border-indigo-500'>
-          Python
-        </div>
-        <div className='text-3xl font-bold p-3 border-2 rounded-xl ml-4 border-amber-700'>
-          SQL
-        </div>
-        <div className='text-3xl font-bold p-3 border rounded-xl ml-4 border-white'>
-          DSA
-        </div>
-        <div className='text-3xl font-bold p-3 border-2 rounded-xl ml-4 border-green-500'>
-          Javascript
-        </div>
-        <div className='text-3xl font-bold p-3 border-2 rounded-xl ml-4 border-yellow-500'>
-          ReactJS
-        </div>
-        <div className='text-3xl font-bold p-3 border-2 rounded-xl ml-4 border-orange-500'>
-          MongoDB
+    <div className='bg-black  p-10 pl-7 pr-7 md:pl-10 md:pr-10'>
+      <h1 className='text-white text-4xl font-bold'>Important Links</h1>
+      <div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 pt-10 gap-4'>
+          <div className='h-[60px] lg:h-[70px] w-[100%] bg-gradient-to-r bg-white rounded-4xl flex justify-center items-center '>
+            <p className='text-xl md:text-2xl font-bold text-black'>C++</p>
+          </div>
+
+          <div className='h-[60px] lg:h-[70px] w-[100%] bg-gradient-to-r bg-white rounded-4xl flex justify-center items-center '>
+            <p className='text-xl md:text-2xl font-bold text-black'>React Js</p>
+          </div>
+
+          <div className='h-[60px] lg:h-[70px] w-[100%] bg-gradient-to-r bg-white rounded-4xl flex justify-center items-center '>
+            <p className='text-xl md:text-2xl font-bold text-black'>Python</p>
+          </div>
+
+          <div className='h-[60px] lg:h-[70px] w-[100%] bg-gradient-to-r bg-white rounded-4xl flex justify-center items-center '>
+            <p className='text-xl md:text-2xl font-bold text-black'>Mongo DB</p>
+          </div>
+
+          <div className='h-[60px] lg:h-[70px] w-[100%] bg-gradient-to-r bg-white rounded-4xl flex justify-center items-center '>
+            <p className='text-xl md:text-2xl font-bold text-black'>NodeJs</p>
+          </div>
+
+          <div className='h-[60px] lg:h-[70px] w-[100%] bg-gradient-to-r bg-white rounded-4xl flex justify-center items-center '>
+            <p className='text-xl md:text-2xl font-bold text-black'>DSA</p>
+          </div>
+
+          <div className='h-[60px] lg:h-[70px] w-[100%] bg-gradient-to-r bg-white rounded-4xl flex justify-center items-center '>
+            <p className='text-xl md:text-2xl font-bold text-black'>SQL</p>
+          </div>
+
+          <div className='h-[60px] lg:h-[70px] w-[100%] bg-gradient-to-r bg-white rounded-4xl flex justify-center items-center '>
+            <p className='text-xl md:text-2xl font-bold text-black'>JavaScript</p>
+          </div>
+          
+
+          
+          
+
+
+          
+
+          
         </div>
       </div>
     </div>
